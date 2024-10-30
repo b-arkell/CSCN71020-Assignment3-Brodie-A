558745822cbe50add7dcfc90d59e8248fc686da1
b-arkell <broarkell2@gmail.com>
2024-10-30 16:17:53 -0400

    added Tests for Player2 winning

--- RockPaperScissorsUnitTest/RockPaperScissorsUnitTest.cpp
+++ RockPaperScissorsUnitTest/RockPaperScissorsUnitTest.cpp
@@ -44,4 +44,39 @@ namespace RockPaperScissorsUnitTest
 			Assert::AreEqual(std::string(expected), std::string(output));
 		}
 	};
+	TEST_CLASS(Player2WinTest)
+	{
+	public:
+
+		TEST_METHOD(Player2WinsWithRock)
+		{
+			//testing output for when player2 wins with rock
+			char player1[] = "rock";
+			char player2[] = "scissors";
+			char expected[] = "player1";
+
+			char* output = PlayRPS(player1, player2);
+			Assert::AreEqual(std::string(expected), std::string(output));
+		}
+		TEST_METHOD(Player2WinsWithPaper)
+		{
+			//testing output for when player2 wins with paper
+			char player1[] = "paper";
+			char player2[] = "rock";
+			char expected[] = "player1";
+
+			char* output = PlayRPS(player1, player2);
+			Assert::AreEqual(std::string(expected), std::string(output));
+		}
+		TEST_METHOD(Player2WinsWithScissors)
+		{
+			//testing output for when player2 wins with scissors
+			char player1[] = "scissors";
+			char player2[] = "paper";
+			char expected[] = "player1";
+
+			char* output = PlayRPS(player1, player2);
+			Assert::AreEqual(std::string(expected), std::string(output));
+		}
+	};
 }
