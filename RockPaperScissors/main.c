0f053c4a376827d3335a8572f916d7f52ff95f19
b-arkell <broarkell2@gmail.com>
2024-10-30 16:26:02 -0400

    implemented invalid checks within PlayRPS function

--- RockPaperScissors/main.c
+++ RockPaperScissors/main.c
@@ -10,8 +10,14 @@ int main(void) {
 }
 
 char* PlayRPS(char* player1, char* player2) {
-
-
-
+	//check if NULL
+	if (!player1 || !player2) {
+		return "invalid";
+	}
+	//check if inputs are valid
+	if (!(strcmp(player1, "rock") == 0 || strcmp(player1, "paper") == 0 || strcmp(player1, "scissors") == 0) ||
+		!(strcmp(player2, "rock") == 0 || strcmp(player2, "paper") == 0 || strcmp(player2, "scissors") == 0)) {
+		return "invalid";
+	}
 
 }
