bb188b7c997018bdfa6e397391a5cb62ea1279bc
b-arkell <broarkell2@gmail.com>
2024-10-30 16:29:16 -0400

    implemented checks if Player1 or Player2 wins in PlayRPS function

--- RockPaperScissors/main.c
+++ RockPaperScissors/main.c
@@ -24,4 +24,12 @@ char* PlayRPS(char* player1, char* player2) {
 
 		return "draw";
 	}
+	//check if player1 wins
+	if ((strcmp(player1, "rock") == 0 && strcmp(player2, "scissors") == 0) ||
+		(strcmp(player1, "paper") == 0 && strcmp(player2, "rock") == 0) ||
+		(strcmp(player1, "scissors") == 0 && strcmp(player2, "paper") == 0)) {
+		return "player1";
+	}
+
+	return "player2";
 }
